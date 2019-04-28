cd688b18c4c9c56af74a2f18c82f54978df6df33
tringcooler <tringcooler@users.noreply.github.com>
2019-04-28 21:25:12 +0800

    standard commit

--- nmtest.js
+++ nmtest.js
@@ -3,16 +3,40 @@ var m_canvas = (function() {
     
 	function m_canvas(parent, size = [300, 300]) {
         var canvas = $('<canvas>');
-        canvas.width(size[0]);
-        canvas.height(size[1]);
+        //canvas.width(size[0]);
+        //canvas.height(size[1]);
+        //issue for canvas resize
+        canvas.attr('width', size[0]);
+        canvas.attr('height', size[1]);
         parent.append(canvas);
         this.elem = canvas;
-        this.size = size;
+        this.cvs = canvas.get(0)
+        this.sync_meta();
 	}
     
+    m_canvas.prototype.sync_meta = function() {
+        this.ctx = this.cvs.getContext('2d');
+        this.width = this.cvs.width;
+        this.height = this.cvs.height;
+    };
+    
 	m_canvas.prototype.draw = function(mmv) {
-        
-    }
+        idata = this.ctx.createImageData(this.width, this.height);
+        var ilen = idata.width * idata.height * 4;
+        for(var i = 0; i < ilen; i += 4) {
+            if(mmv.length == 3) {
+                idata.data[i + 0] = mmv[0][i/4] * 255;
+                idata.data[i + 1] = mmv[1][i/4] * 255;
+                idata.data[i + 2] = mmv[2][i/4] * 255;
+            } else {
+                idata.data[i + 0] = mmv[i/4] * 255;
+                idata.data[i + 1] = mmv[i/4] * 255;
+                idata.data[i + 2] = mmv[i/4] * 255;
+            }
+            idata.data[i + 3] = 255;
+        }
+        this.ctx.putImageData(idata, 0, 0);
+    };
     
     return m_canvas;
     
@@ -20,13 +44,21 @@ var m_canvas = (function() {
 
 var m_matrix = (function() {
     
-	function m_matrix(...srcm) {
+	function m_matrix(srcm) {
         this.srcm = srcm;
 	}
     
     m_matrix.prototype.e_op = function(op, a, b) {
         if( !(a instanceof Array) && !(b instanceof Array) ) {
-            return eval(a + op + b);
+            if(op.length > 1) {
+                if(b === null) {
+                    return eval(op + '(' + a + ')');
+                } else {
+                    return eval(op + '(' + a + ',' + b + ')');
+                }
+            } else {
+                return eval(a + op + b);
+            }
         } else if(!(a instanceof Array)) {
             a = Array(b.length).fill(a);
         } else if(!(b instanceof Array)) {
@@ -38,12 +70,43 @@ var m_matrix = (function() {
             r.push(this.e_op(op, a[i], b[i]));
         }
         return r;
-    }
+    };
     
 	m_matrix.prototype.bright = function() {
-        
-    }
+        return this.e_op('Math.sqrt',
+            this.e_op('+',
+            this.e_op('+',
+                this.e_op('Math.pow', this.srcm[0], 2),
+                this.e_op('Math.pow', this.srcm[1], 2)),
+                this.e_op('Math.pow', this.srcm[2], 2)),
+            null);
+    };
     
     return m_matrix;
     
 })();
+
+var nm_sphere = function(rad, size, cent = null) {
+    if(cent == null) {
+        cent = size.map(a=>Math.floor(a/2));
+    }
+    var calc_v = function(x, y, r) {
+        var d2 = x * x + y * y;
+        var r2 = r * r;
+        var v;
+        if(d2 > r2) {
+            v = [0, 0, rad];
+        } else {
+            v = [x, y, Math.sqrt(r2 - d2)];
+        }
+        return v.map(a=>a/r);
+    }
+    var r = [[], [], []];
+    for(var x = 0; x < size[0]; x ++) {
+        for(var y = 0; y < size[1]; y ++) {
+            var d = calc_v(x - cent[0], y - cent[1], rad);
+            [0,1,2].map(i=>r[i].push(d[i]));
+        }
+    }
+    return r;
+}
